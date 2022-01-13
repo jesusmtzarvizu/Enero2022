4a27d2bbf43f477e89394e236b9220bdb1e1d555
jesusmtzarvizu <jesusmtzarvizu@users.noreply.github.com>
2022-01-12 21:53:14 -0700

    Modifying the main method

--- binarysearchtree/src/main/java/com/example/App.java
+++ binarysearchtree/src/main/java/com/example/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "-Binary Search Tree-" );
+        
+        binarySearchTree arbol=new binarySearchTree();
+        arbol.insert(8);
+        arbol.insert(2);
+        arbol.insert(3);
+      
+        System.out.println("arbol:"+arbol);
     }
 }
